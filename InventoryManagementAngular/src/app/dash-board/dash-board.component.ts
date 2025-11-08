51f00848917f8b9f2c7dcf74c7daf28ad21baf4a
Moutasem Alzoubi <moutasemalzoubi1@gmail.com>
2025-11-08 20:40:50 +0300

    Update sidebar menu: Dashboard, Settings, Warehouses

--- InventoryManagementAngular/src/app/dash-board/dash-board.component.ts
+++ InventoryManagementAngular/src/app/dash-board/dash-board.component.ts
@@ -1,37 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { SignUp } from '../Model/SignUp';
 import { UserService } from '../Service/UserService';
 import { Router } from '@angular/router';
+
+declare var initMenu: any; // إعلان عن الدالة الموجودة في script.js
 
 @Component({
   selector: 'app-dash-board',
   templateUrl: './dash-board.component.html',
   styleUrls: ['./dash-board.component.css']
 })
-export class DashBoardComponent implements OnInit{
-  constructor(private router:Router,
-              private userService:UserService
-  ){}
+export class DashBoardComponent implements OnInit, AfterViewInit {
+
+  constructor(private router: Router,
+              private userService: UserService) {}
+
   userName: string | null = '';
   role: string | null = '';
-   ngOnInit(): void {
+
+  ngOnInit(): void {
     this.userName = localStorage.getItem('userName');
     this.role = localStorage.getItem('role');
   }
 
+  ngAfterViewInit(): void {
+    // استدعاء initMenu بعد تحميل الـ DOM
+    initMenu();
+  }
 
-
-  LogOut(){
+  LogOut() {
     this.userService.logout();
-    this.router.navigate([''])
+    this.router.navigate(['']);
   }
 
-
-
-  
-
-  
-    
-
-  }
-
+}
--- InventoryManagementAngular/src/assets/js/script.js
+++ InventoryManagementAngular/src/assets/js/script.js
@@ -13,44 +13,30 @@ Version      : 1.0
 	var $pageWrapper = $('.page-wrapper');
 	var $slimScrolls = $('.slimscroll');
 	
-	// Sidebar
-	
-	var Sidemenu = function() {
-		this.$menuItem = $('#sidebar-menu a');
-	};
-	
-	function init() {
-		var $this = Sidemenu;
-		$('#sidebar-menu a').on('click', function(e) {
-			if($(this).parent().hasClass('submenu')) {
-				e.preventDefault();
-			}
-			if(!$(this).hasClass('subdrop')) {
-				$('ul', $(this).parents('ul:first')).slideUp(350);
-				$('a', $(this).parents('ul:first')).removeClass('subdrop');
-				$(this).next('ul').slideDown(350);
-				$(this).addClass('subdrop');
-			} else if($(this).hasClass('subdrop')) {
-				$(this).removeClass('subdrop');
-				$(this).next('ul').slideUp(350);
-			}
-		});
-		$('#sidebar-menu ul li.submenu a.active').parents('li:last').children('a:first').addClass('active').trigger('click');
-	}
-	
-	// Sidebar Initiate
-	init();
-	
-	// Mobile menu sidebar overlay
-	
-	$('body').append('<div class="sidebar-overlay"></div>');
-	$(document).on('click', '#mobile_btn', function() {
-		$wrapper.toggleClass('slide-nav');
-		$('.sidebar-overlay').toggleClass('opened');
-		$('html').addClass('menu-opened');
-		return false;
-	});
-	
+	// ========================
+// Sidebar - Menu (Multi-Level Angular Ready)
+// ========================
+window.initMenu = function() {
+    $('#sidebar-menu a').off('click').on('click', function(e) {
+        if ($(this).parent().hasClass('submenu')) {
+            e.preventDefault();
+        }
+        if (!$(this).hasClass('subdrop')) {
+            $('ul', $(this).parents('ul:first')).slideUp(350);
+            $('a', $(this).parents('ul:first')).removeClass('subdrop');
+            $(this).next('ul').slideDown(350);
+            $(this).addClass('subdrop');
+        } else if ($(this).hasClass('subdrop')) {
+            $(this).removeClass('subdrop');
+            $(this).next('ul').slideUp(350);
+        }
+    });
+    $('#sidebar-menu ul li.submenu a.active').parents('li:last').children('a:first').addClass('active').trigger('click');
+};
+
+// تفعيل المنيو أول مرة عند تحميل الصفحة
+initMenu();
+
 	// toggle-password
 	
 	if($('.toggle-password').length > 0) {
